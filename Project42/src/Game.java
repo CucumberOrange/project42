d284070ef4c84648535c15d4474ceb2b0360048d
CucumberOrange <64110857+CucumberOrange@users.noreply.github.com>
2020-12-10 09:23:46 -0800

    Update Game.java

--- Project42/src/Game.java
+++ Project42/src/Game.java
@@ -214,7 +214,7 @@ public class Game extends Canvas {
 
           // otherwise add a shot
           lastFire = System.currentTimeMillis();
-          ShotEntity shot = new ShotEntity(this, "sprites/shot.gif", 
+          ShotEntity shot = new ShotEntity(this, "sprites/redShot.gif", 
                             ship.getX(), ship.getY());
           entities.add(shot);
         } // tryToFire
